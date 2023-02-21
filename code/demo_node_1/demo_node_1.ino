5ae2c57566f20fde7349129b148bb844bb0fbd72
luthfipratamaps <luthfipratamaps@gmail.com>
2023-02-21 15:16:13 +0700

    added code

---
+++ code/demo_node_1/demo_node_1.ino
@@ -0,0 +1,94 @@
+/* demo_node_1.ino
+ * Pair with demo_gateway_1.ino
+ * Last edited: 2023-02-21
+ *
+ * By: Luthfi Pratama
+ */
+
+//Importing libraries
+//LORA libraries
+#include <RHReliableDatagram.h>
+#include <RH_RF95.h>
+#include <SPI.h>
+
+// LORA settings
+#define RFM95_CS 10
+#define RFM95_RST 9
+#define RFM95_INT 2
+
+#define RTU_ADDRESS 11
+#define GATEWAY_ADDRESS 10
+
+//Creating objects and instances
+//Singleton instance of the radio driver
+RH_RF95 driver(RFM95_CS, RFM95_INT);
+//Class to manage message delivery and receipt, using the driver declared above
+RHReliableDatagram manager(driver, RTU_ADDRESS);
+
+uint8_t data[128];
+uint8_t buf[RH_RF95_MAX_MESSAGE_LEN];
+String Gateway_Command = String("REQ_RTU13");
+
+float sensor1, sensor2, sensor3, sensor4;
+String dataset;
+
+void setup() {  
+  //Serial setup
+  Serial.begin(9600);
+  while (!Serial) ; // Wait for serial port to be available
+  
+  //LORA setup
+  pinMode(RFM95_RST, OUTPUT);
+  digitalWrite(RFM95_RST, HIGH);
+  
+  // manual reset
+  digitalWrite(RFM95_RST, LOW);
+  delay(10);
+  digitalWrite(RFM95_RST, HIGH);
+  delay(10);
+  
+  if (!manager.init()) 
+    Serial.println("init failed");
+
+  driver.setFrequency(440.0);
+  driver.setTxPower(18, false);
+}
+
+void loop() {
+
+  sensor1 = 32.23;
+  sensor2 = 76.89;
+  sensor3 = 80.12;
+  sensor4 = 112.74;
+
+  if (manager.available()){
+    // Wait for a message addressed to this node
+    uint8_t len = sizeof(buf);
+    uint8_t from;
+    if (manager.recvfromAck(buf, &len, &from)){
+      Serial.print("got request from : 0x");
+      Serial.print(from, HEX);
+      Serial.print(": ");
+      Serial.println((char*)buf);
+
+      if(Gateway_Command == (char*)buf){
+        Serial.println("Sending response");
+
+        dataset = String(RTU_ADDRESS);
+        dataset += "," + String(sensor1);
+        dataset += "," + String(sensor2);
+        dataset += "," + String(sensor3);
+        dataset += "," + String(sensor4);
+        dataset.toCharArray(data, 128);
+
+        Serial.println(dataset);
+        Serial.println("Done...\n");
+
+
+      // Send a reply back to gateway
+      if (!manager.sendtoWait(data, sizeof(data), from)) 
+        Serial.println("sendtoWait failed");
+      }
+    }
+  }
+}
